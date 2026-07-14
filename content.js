3d8ded2d9f746dbd3cc5de357dd9f1683fd9e634
davideveryday <david.castrosp80@gmail.com>
2026-07-14 16:00:30 -0300

    get synonym for selected word

--- content.js
+++ content.js
@@ -1,1 +1,43 @@
-console.log("hello world");
+class SynonymProvider {
+    #dictionary = null;
+
+    async init() {
+        try {
+            const fileUrl = chrome.runtime.getURL("synonyms.json");
+            const response = await fetch(fileUrl);
+            this.#dictionary = await response.json();
+        } catch (error) {
+            console.error("Error fetching local resource:", error);
+        }
+    }
+
+    getSynonym(word) {
+        return this.#dictionary[word];
+    }
+}
+
+
+function parseMouseSelection(input) {
+    const text = input.toString().trim();
+    if (text.length < 0) return null;
+    const lastSelectedWord = text
+        .split(/\s+/)
+        .pop()
+        ?.replace(/[.,;:!?()"']/g, "");
+
+    return lastSelectedWord;
+}
+
+function handleSelection(synonymProvider) {
+    const word = parseMouseSelection(window.getSelection());
+
+    console.log(word + " ---->" + (synonymProvider.getSynonym(word) ?? ""));
+}
+
+async function main() {
+    const synonymProvider = new SynonymProvider();
+    await synonymProvider.init();
+    document.addEventListener("mouseup", () => handleSelection(synonymProvider));
+}
+
+main();
